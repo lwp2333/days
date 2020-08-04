dc06f9b705fe55595843205741edefcc956fe7bf
lwp2333 <weiping@yscredit.com>
2020-08-04 17:11:12 +0800

    网络接口错误提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -22,43 +22,53 @@ service.interceptors.request.use(
 
 // 响应拦截
 
-service.interceptors.response.use(res => {
-  if (res.data.code === 200) {
-    // 接口请求成功
-    return res.data.data
-  }
-  if (res.data.code === 201 || res.data.code === 203) {
-    // 删除，修改记录成功
-    Notify({
-      type: 'success',
-      message: res.data.message
-    })
-    return res.data.data
-  }
-  if (res.data.code === 202 || res.data.code === 203) {
-    // 删除，修改记录失败
+service.interceptors.response.use(
+  res => {
+    if (res.data.code === 200) {
+      // 接口请求成功
+      return res.data.data
+    }
+    if (res.data.code === 201 || res.data.code === 203) {
+      // 删除，修改记录成功
+      Notify({
+        type: 'success',
+        message: res.data.message
+      })
+      return res.data.data
+    }
+    if (res.data.code === 202 || res.data.code === 203) {
+      // 删除，修改记录失败
+      Notify({
+        type: 'danger',
+        message: res.data.message
+      })
+      return Promise.reject(res.data.message)
+    }
+    if (res.data.code === 301) {
+      // 请求失败，原因未知
+      Notify({
+        type: 'danger',
+        message: res.data.message
+      })
+      return Promise.reject(res.data.message)
+    }
+    if (res.data.code === 302) {
+      // 请求失败，字段缺失
+      Notify({
+        type: 'warning',
+        message: res.data.message
+      })
+      return Promise.reject(res.data.message)
+    }
+  },
+  err => {
+    // 接口调用失败
     Notify({
       type: 'danger',
-      message: res.data.message
+      message: err.message
     })
-    return Promise.reject(res.data.message)
+    return Promise.reject(err.message)
   }
-  if (res.data.code === 301) {
-    // 请求失败，原因未知
-    Notify({
-      type: 'danger',
-      message: res.data.message
-    })
-    return Promise.reject(res.data.message)
-  }
-  if (res.data.code === 302) {
-    // 请求失败，字段缺失
-    Notify({
-      type: 'warning',
-      message: res.data.message
-    })
-    return Promise.reject(res.data.message)
-  }
-})
+)
 
 export default service
