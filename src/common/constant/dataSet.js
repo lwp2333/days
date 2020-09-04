29f5838087ef31a3f77aea1aee745ce20e803ce4
lwp2333 <weiping@yscredit.com>
2020-09-04 18:05:36 +0800

    词云数据修改

--- src/common/constant/dataSet.js
+++ src/common/constant/dataSet.js
@@ -1,151 +1,151 @@
 export default [
   {
-    x: 'China',
+    x: '幻想岁月无声，',
     value: 1383220000,
     category: 'asia'
   },
   {
-    x: 'India',
+    x: '从来酿酒的人',
     value: 1316000000,
     category: 'asia'
   },
   {
-    x: 'United States',
+    x: '百年之后',
     value: 324982000,
     category: 'america'
   },
   {
-    x: 'Indonesia',
+    x: '合于一坟',
     value: 263510000,
     category: 'asia'
   },
   {
-    x: 'Brazil',
+    x: '，分外清醒',
     value: 207505000,
     category: 'america'
   },
   {
-    x: 'Pakistan',
+    x: '独善其身',
     value: 196459000,
     category: 'asia'
   },
   {
-    x: 'Nigeria',
+    x: '做一条河的清水',
     value: 191836000,
     category: 'africa'
   },
   {
-    x: 'Bangladesh',
+    x: '悄悄喜欢船上的人',
     value: 162459000,
     category: 'asia'
   },
   {
-    x: 'Russia',
+    x: '可我偏偏没再见过你',
     value: 146804372,
     category: 'europe'
   },
   {
-    x: 'Japan',
+    x: '我要划船流进你心里',
     value: 126790000,
     category: 'asia'
   },
   {
-    x: 'Mexico',
+    x: '可我整夜的数着星星',
     value: 123518000,
     category: 'america'
   },
   {
-    x: 'Ethiopia',
+    x: '谁也没能够道破天机',
     value: 104345000,
     category: 'africa'
   },
   {
-    x: 'Philippines',
+    x: '我多想再多陪你一场',
     value: 104037000,
     category: 'asia'
   },
   {
-    x: 'Egypt',
+    x: '把前半生的风景对你讲',
     value: 93013300,
     category: 'africa'
   },
   {
-    x: 'Vietnam',
+    x: '枯木逢春',
     value: 92700000,
     category: 'asia'
   },
   {
-    x: 'Germany',
+    x: '枯木尚未逢春',
     value: 82800000,
     category: 'europe'
   },
   {
-    x: 'Democratic Republic of the Congo',
+    x: '剩不多也不少',
     value: 82243000,
     category: 'africa'
   },
   {
-    x: 'Iran',
+    x: '还能忘掉',
     value: 80135400,
     category: 'asia'
   },
   {
-    x: 'Turkey',
+    x: '我好像在哪见过你',
     value: 79814871,
     category: 'asia'
   },
   {
-    x: 'Thailand',
+    x: '我心非石不可转',
     value: 68298000,
     category: 'asia'
   },
   {
-    x: 'France',
+    x: '我心非席不可卷',
     value: 67013000,
     category: 'europe'
   },
   {
-    x: 'United Kingdom',
+    x: '空凝眸',
     value: 65110000,
     category: 'europe'
   },
   {
-    x: 'Italy',
+    x: '情字深浅无解',
     value: 60599936,
     category: 'europe'
   },
   {
-    x: 'Tanzania',
+    x: 'Vue 3.0',
     value: 56878000,
     category: 'africa'
   },
   {
-    x: 'South Africa',
+    x: 'react',
     value: 55908000,
     category: 'africa'
   },
   {
-    x: 'Myanmar',
+    x: 'Dva',
     value: 54836000,
     category: 'asia'
   },
   {
-    x: 'South Korea',
+    x: 'angular',
     value: 51446201,
     category: 'asia'
   },
   {
-    x: 'Colombia',
+    x: 'vue 2.6.10',
     value: 49224700,
     category: 'america'
   },
   {
-    x: 'Kenya',
+    x: 'Vuex',
     value: 48467000,
     category: 'africa'
   },
   {
-    x: 'Spain',
+    x: 'Flutter',
     value: 46812000,
     category: 'europe'
   }
